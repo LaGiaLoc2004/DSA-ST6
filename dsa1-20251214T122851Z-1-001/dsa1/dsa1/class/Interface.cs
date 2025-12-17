86798c2c01fb679be32cbb78e78731dc1b2faa38
lagis <172578465+LaGiaLoc2004@users.noreply.github.com>
2025-12-17 20:45:42 +0700

    sua loi file .vs

---
+++ dsa1-20251214T122851Z-1-001/dsa1/dsa1/class/Interface.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+public class Interface
+{
+    public interface ITraversable<T>
+    {
+        bool HasNext();
+        T Next();
+    }
+
+    public class ArrayTraversable<T> : ITraversable<T>
+    {
+        private T[] data;
+        private int index = 0;
+
+        public ArrayTraversable(T[] data)
+        {
+            this.data = data;
+        }
+
+        public bool HasNext()
+        {
+            return index < data.Length;
+        }
+
+        public T Next()
+        {
+            return data[index++];
+        }
+    }
+
+}
---
+++ dsa1-20251214T122851Z-1-001/dsa1/dsa1/class/ThuatToanTimKiem.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public class ThuatToanTimKiem
+{
+	public class  BFSFinder<T>
+	{
+        private MyStack<T> chieuVao = new MyStack<T>();
+        private MyStack<T> chieuRa = new MyStack<T>();
+
+        public void Search(ITraversable<T> origin, Action<T> visit)
+        {
+            // Đưa dữ liệu vào chieuVao
+            while (origin.HasNext())
+            {
+                chieuVao.Push(origin.Next());
+            }
+
+            // BFS mô phỏng FIFO
+            while (!chieuVao.IsEmpty() || !chieuRa.IsEmpty())
+            {
+                if (chieuRa.IsEmpty())
+                {
+                    while (!chieuVao.IsEmpty())
+                    {
+                        chieuRa.Push(chieuVao.Pop());
+                    }
+                }
+
+                visit(chieuRa.Pop());
+            }
+        }
+    }
+
+    public class DFSFinder<T>
+    {
+         public bool Search(ITraversable<T> origin, Action<T> visit)
+         {
+            MyStack<T> tempStack = new MyStack<T>();
+
+            // Đưa dữ liệu vào stack tạm thời
+            while (origin.HasNext())
+            {
+                tempStack.Push(origin.Next());
+            }
+
+            while (!tempStack.IsEmpty())
+            {
+                visit(tempStack.Pop());
+            }
+         }
+    }
+
+    public class LinearSearchFinder<T>
+    {
+        public bool Search(ITraversable<T> origin, T target)
+        {
+            MyStack<T> tempStack = new MyStack<T>();
+
+            // Đưa dữ liệu vào stack
+            while (origin.HasNext())
+            {
+                tempStack.Push(origin.Next());
+            }
+
+            // Tìm kiếm
+            while (!tempStack.IsEmpty())
+            {
+                if (Equals(tempStack.Pop(), target))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+}
