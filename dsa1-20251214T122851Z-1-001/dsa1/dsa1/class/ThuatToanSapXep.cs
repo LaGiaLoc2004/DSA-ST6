faf8254dca94aa85444f5ee95d7dd42a0ade2b94
lythienphuong172 <114716819+lythienphuong172@users.noreply.github.com>
2025-12-18 14:04:50 +0700

    Update ThuatToanSapXep.cs

--- dsa1-20251214T122851Z-1-001/dsa1/dsa1/class/ThuatToanSapXep.cs
+++ dsa1-20251214T122851Z-1-001/dsa1/dsa1/class/ThuatToanSapXep.cs
@@ -1,111 +1,117 @@
-﻿using System;
+using dsa1;
 
 public class ThuanToanSapXep
 {
-	public static void InsertionSort(MyStack<int> stack)
-	{
-	    MyStack<int> sorted = new MyStack<int>(stack.Capacity);
-	
-	    while (!stack.IsEmpty())
-	    {
-	        int temp = stack.Pop();
-	
-	        // Dời các phần tử lớn hơn temp về stack gốc
-	        while (!sorted.IsEmpty() && sorted.Peek() > temp)
-	        {
-	            stack.Push(sorted.Pop());
-	        }
-	
-	        sorted.Push(temp);
-	    }
-	
-	    // Đưa lại kết quả về stack ban đầu
-	    while (!sorted.IsEmpty())
-	        stack.Push(sorted.Pop());
-	}
+    // INSERTION SORT
+    public static void InsertionSort<T>(MyStack<T> stack)
+        where T : IComparable<T>
+    {
+        MyStack<T> sorted = new MyStack<T>(stack.Capacity);
 
-	public static void SelectionSort(MyStack<int> stack)
-	{
-	    MyStack<int> result = new MyStack<int>(stack.Capacity);
-	    MyStack<int> tempStack = new MyStack<int>(stack.Capacity);
-	
-	    while (!stack.IsEmpty())
-	    {
-	        int min = stack.Pop();
-	        tempStack.Push(min);
-	
-	        // Tìm min
-	        while (!stack.IsEmpty())
-	        {
-	            int x = stack.Pop();
-	            tempStack.Push(x);
-	            if (x < min) min = x;
-	        }
-	
-	        bool removed = false;
-	
-	        // Loại 1 lần min
-	        while (!tempStack.IsEmpty())
-	        {
-	            int x = tempStack.Pop();
-	            if (x == min && !removed)
-	            {
-	                removed = true;
-	                continue;
-	            }
-	            stack.Push(x);
-	        }
-	
-	        result.Push(min);
-	    }
-	
-	    // Đưa lại về stack ban đầu
-	    while (!result.IsEmpty())
-	        stack.Push(result.Pop());
-	}
+        while (!stack.IsEmpty())
+        {
+            T temp = stack.Pop();
 
-	private static MyStack<int> Merge(MyStack<int> left, MyStack<int> right)
-	{
-	    MyStack<int> result = new MyStack<int>(left.Capacity + right.Capacity);
-	    MyStack<int> temp = new MyStack<int>(left.Capacity + right.Capacity);
-	
-	    while (!left.IsEmpty() && !right.IsEmpty())
-	    {
-	        if (left.Peek() <= right.Peek())
-	            temp.Push(left.Pop());
-	        else
-	            temp.Push(right.Pop());
-	    }
-	
-	    while (!left.IsEmpty()) temp.Push(left.Pop());
-	    while (!right.IsEmpty()) temp.Push(right.Pop());
-	
-	    // đảo lại để đúng thứ tự
-	    while (!temp.IsEmpty())
-	        result.Push(temp.Pop());
-	
-	    return result;
-	}
-	
-	public static MyStack<int> MergeSort(MyStack<int> stack)
-	{
-	    if (stack.Count() <= 1)
-	        return stack;
-	
-	    MyStack<int> left = new MyStack<int>(stack.Capacity);
-	    MyStack<int> right = new MyStack<int>(stack.Capacity);
-	
-	    int mid = stack.Count() / 2;
-	
-	    for (int i = 0; i < mid; i++)
-	        left.Push(stack.Pop());
-	
-	    while (!stack.IsEmpty())
-	        right.Push(stack.Pop());
-	
-	    left = MergeSort(left);
-	    right = MergeSort(right);
-	
-	    return Merge(left, right);
-	}
+            while (!sorted.IsEmpty() &&
+                   sorted.Peek().CompareTo(temp) > 0)
+            {
+                stack.Push(sorted.Pop());
+            }
+
+            sorted.Push(temp);
+        }
+
+        while (!sorted.IsEmpty())
+            stack.Push(sorted.Pop());
+    }
+
+    //SELECTION SORT
+    public static void SelectionSort<T>(MyStack<T> stack)
+        where T : IComparable<T>
+    {
+        MyStack<T> result = new MyStack<T>(stack.Capacity);
+        MyStack<T> tempStack = new MyStack<T>(stack.Capacity);
+
+        while (!stack.IsEmpty())
+        {
+            T min = stack.Pop();
+            tempStack.Push(min);
+
+            while (!stack.IsEmpty())
+            {
+                T x = stack.Pop();
+                tempStack.Push(x);
+
+                if (x.CompareTo(min) < 0)
+                    min = x;
+            }
+
+            bool removed = false;
+
+            while (!tempStack.IsEmpty())
+            {
+                T x = tempStack.Pop();
+
+                if (!removed && x.CompareTo(min) == 0)
+                {
+                    removed = true;
+                    continue;
+                }
+
+                stack.Push(x);
+            }
+
+            result.Push(min);
+        }
+
+        while (!result.IsEmpty())
+            stack.Push(result.Pop());
+    }
+
+    // MERGE SORT
+    public static MyStack<T> MergeSort<T>(MyStack<T> stack)
+        where T : IComparable<T>
+    {
+        if (stack.Count() <= 1)
+            return stack;
+
+        int mid = stack.Count() / 2;
+
+        MyStack<T> left = new MyStack<T>(stack.Capacity);
+        MyStack<T> right = new MyStack<T>(stack.Capacity);
+
+        for (int i = 0; i < mid; i++)
+            left.Push(stack.Pop());
+
+        while (!stack.IsEmpty())
+            right.Push(stack.Pop());
+
+        left = MergeSort(left);
+        right = MergeSort(right);
+
+        return Merge(left, right);
+    }
+
+    private static MyStack<T> Merge<T>(MyStack<T> left, MyStack<T> right)
+        where T : IComparable<T>
+    {
+        MyStack<T> temp = new MyStack<T>(left.Capacity + right.Capacity);
+        MyStack<T> result = new MyStack<T>(left.Capacity + right.Capacity);
+
+        while (!left.IsEmpty() && !right.IsEmpty())
+        {
+            if (left.Peek().CompareTo(right.Peek()) <= 0)
+                temp.Push(left.Pop());
+            else
+                temp.Push(right.Pop());
+        }
+
+        while (!left.IsEmpty()) temp.Push(left.Pop());
+        while (!right.IsEmpty()) temp.Push(right.Pop());
+
+        while (!temp.IsEmpty())
+            result.Push(temp.Pop());
+
+        return result;
+    }
 }
